dc5c84dc049e4936a1769cc164e3469386ea7133
sudhanshuShekhar <147860860+sudhanshushekhar1982@users.noreply.github.com>
2024-10-03 20:45:22 +0530

    new commit for recurssion

--- recursion/f2.java
+++ recursion/f2.java
@@ -12,10 +12,18 @@ public class f2 {
     }
 
     public static int factorial(int n) {
-        if(n == 0) {
+        /*if(n == 0) {
             return 1;
         }
         return n*factorial(n-1);
+        */
+        
+        if(n == 0) {
+            return 1;
+        }
+        int fnm1 = factorial(n-1);
+        int fn = n * factorial(n-1);
+        return fn;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
