8774e56d97f4387df1478e8d0af6b199d81e9f85
sudhanshuShekhar <147860860+sudhanshushekhar1982@users.noreply.github.com>
2024-10-05 10:25:50 +0530

    remove duplicates

--- recursion/removeDuplicate.java
+++ recursion/removeDuplicate.java
@@ -2,8 +2,23 @@ package recursion;
 import java.util.*;
 import java.lang.*;
 public class removeDuplicate {
-    public static 
+    public static void removeDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
+        // base case
+        if(idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        //kaam
+        char currChar = str.charAt(idx);
+        if(map[currChar - 'a'] == true) {
+            removeDuplicates(str, idx+1, newStr, map);
+        } else {
+            map[currChar - 'a'] = true;
+            removeDuplicates(str, idx+1, newStr.append(currChar), map);
+        }
+    } 
     public static void main(String [] args) {
-
+        String str = "apnacollege";
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
     }
 }
