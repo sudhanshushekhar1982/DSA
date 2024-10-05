ef358b6424baa6d3ce626ceedd2b92aaf828952b
sudhanshuShekhar <147860860+sudhanshushekhar1982@users.noreply.github.com>
2024-10-05 10:57:25 +0530

    commited

--- recursion/removeDuplicate.java
+++ recursion/removeDuplicate.java
@@ -17,8 +17,37 @@ public class removeDuplicate {
             removeDuplicates(str, idx+1, newStr.append(currChar), map);
         }
     } 
+    // public static void removeduplicate(String str,int idx,StringBuilder newStr,boolean map[]) {
+    //     if(idx == str.length()) {
+    //         System.out.println(newStr);
+    //         return;
+    //     }
+    //     //kaam
+    //     char currChar = str.charAt(idx);
+    //     if(map[currChar - 'a'] == true) {
+    //         removeduplicate(str, idx+1, newStr, map);
+    //     } else {
+    //         map[currChar - 'a'] = true;
+    //         removeduplicate(str, idx+1, newStr.append(currChar), map);
+    //     }
+    // }
+    // public static void remDup(String str , int idx,StringBuilder newStr,boolean map[]) {
+    //     //base case
+    //     if(idx == str.length()) {
+    //         System.out.println(newStr);
+    //         return;
+    //     }
+    //     //kaam 
+    //     char currChar = str.charAt(idx);
+    //     if(map[currChar - 'a'] == true) {
+    //         remDup(str, idx+1, newStr, map);
+    //     } else {
+    //         map[currChar - 'a'] = true;
+    //         remDup(str, idx+1, newStr.append(currChar), map);
+    //     }
+    //}
     public static void main(String [] args) {
-        String str = "apnacollege";
+        String str = "dheerajshandilya";
         removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
     }
 }
