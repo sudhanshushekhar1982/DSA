1c27b3e222eb0f530b1d713e079dfe7fb415e8cb
sudhanshuShekhar <147860860+sudhanshushekhar1982@users.noreply.github.com>
2024-10-05 11:30:30 +0530

    new commit completde

--- recursion/removeDuplicate.java
+++ recursion/removeDuplicate.java
@@ -46,6 +46,22 @@ public class removeDuplicate {
     //         remDup(str, idx+1, newStr.append(currChar), map);
     //     }
     //}
+
+    public static void removeDup(String str,int idx,StringBuilder newStr,boolean map[]) {
+        //base case
+        if(idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        //kaam kya hoga
+        char currChar = str.charAt(idx);
+        if(map[currChar - 'a'] == true) {
+            removeDup(str, idx+1, newStr, map);
+        } else {
+            map[currChar - 'a'] = true;
+            removeDup(str, idx+1, newStr.append(currChar), map);
+        }
+    }
     public static void main(String [] args) {
         String str = "dheerajshandilya";
         removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
