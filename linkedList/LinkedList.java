ceb3805876fc36e54b52cfb06dd917ffd1ab1aaa
sudhanshuShekhar <147860860+sudhanshushekhar1982@users.noreply.github.com>
2024-11-04 18:41:58 +0530

    new commited

--- linkedList/LinkedList.java
+++ linkedList/LinkedList.java
@@ -226,6 +226,30 @@ public class LinkedList {
 
         head = prev;
      }
+     public static void deleteNthfromEnd(int n) {
+        int sz =0;
+        Node temp = head;
+        while(temp != null) {
+            temp = temp.next;
+            sz++;
+        }
+
+        if(n == sz) {
+            head = head.next;
+            return;
+        }
+
+        //sz - n
+        int i =1;
+        int iToFind = sz -n;
+        Node prev = head;
+        while (i < iToFind) {
+            prev = prev.next;
+            i++; 
+        }
+        prev.next = prev.next.next;
+        return;
+     }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.printLL(head);
